25ab9249bfc15181cce1e604aec42411070cfb23
NickSagan <9952728@gmail.com>
2023-01-20 18:29:48 +0100

    add MPB

--- Sources/MPB/MPB.swift
+++ Sources/MPB/MPB.swift
@@ -1,6 +1,26 @@
-public struct MPB {
-    public private(set) var text = "Hello, World!"
+//
+//  MPB.swift
+//  MPBSDK
+//
+//  Created by Nick Sagan on 20.01.2023.
+//
 
-    public init() {
+import UIKit
+
+public class MPB {
+    
+    private init(){}
+    
+    private static var color: UIColor = .lightGray
+    
+    public static func setup(color: UIColor) {
+        MPB.color = color
     }
+    
+    public static func controller() -> UINavigationController {
+        let navBar = UINavigationController(rootViewController: ViewController())
+        navBar.navigationBar.backgroundColor = color
+        return navBar
+    }
+    
 }
