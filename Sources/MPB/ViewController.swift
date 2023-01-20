0810a7985d40841a6deaca096d5f70d03fd6715d
NickSagan <9952728@gmail.com>
2023-01-20 18:30:12 +0100

    add ViewController

---
+++ Sources/MPB/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MPBSDK
+//
+//  Created by Nick Sagan on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
